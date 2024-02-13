fff7074c0018b1589df97ca62f8d3a69af1dd5c8
JainamKarania <jainamkarania05@gmail.com>
2024-02-13 17:59:26 +0530

    Navbar

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCHQtqpqEZk7CWdBTEpNF6IVzMZhCE2gs",
+    authDomain: "the-movies-hub.firebaseapp.com",
+    projectId: "the-movies-hub",
+    storageBucket: "the-movies-hub.appspot.com",
+    messagingSenderId: "283180284683",
+    appId: "1:283180284683:web:52fa29fa2c58164fd5c00e"
+  };
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { HomeScreen } from './Components/HomeScreen'
+
+function App() {
+    return (
+    <>
+      <div className='body'>
+        <HomeScreen/>
+      </div>
+    </>
+  )
+}
+
+export default App
